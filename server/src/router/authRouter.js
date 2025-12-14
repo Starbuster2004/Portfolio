61ccaf7032ca8a6dc7793bfa1da2cce78873eec1
Starbuster2004 <govindrajkotalwar038@gmail.com>
2025-12-14 19:08:32 +0530

    chore: remove temporary admin fix route

--- server/src/router/authRouter.js
+++ server/src/router/authRouter.js
@@ -6,27 +6,6 @@ const { validateBody } = require('../middleware/validate.middleware');
 const { loginSchema } = require('../schemas/validation.schemas');
 const { authRateLimitMiddleware } = require('../middleware/rateLimit.middleware');
 
-// Emergency Fix Route
-router.get('/fix-admin', async (req, res) => {
-    try {
-        const User = require('../models/User');
-        const email = 'govindrajkotalwar@gmail.com';
-        const password = 'Starbuster@2033';
-
-        let user = await User.findOne({ email });
-        if (user) {
-            user.password = password;
-            await user.save();
-            return res.send(`Admin ${email} updated with new password.`);
-        }
-
-        await User.create({ name: 'Admin', email, password, role: 'admin' });
-        res.send(`Admin ${email} created successfully.`);
-    } catch (e) {
-        res.status(500).send(e.toString());
-    }
-});
-
 router.post('/login', authRateLimitMiddleware, validateBody(loginSchema), login);
 router.get('/logout', logout);
 router.get('/me', protect, getMe);
